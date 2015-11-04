c1033da9b787728273fa3ce9646a67d34215bbdc
Francois Guibert <frozax@frozax.com>
2015-11-04 21:20:33 +0100

    add Node::create(std::string name)

--- cocos/2d/CCNode.h
+++ cocos/2d/CCNode.h
@@ -124,6 +124,12 @@ public:
      */
     static Node * create();
 
+	/**
+	 * Allocates and initialized a node with a name.
+	 ] return a initialized node which is marked as autorelease.
+	 */
+    static Node * create(std::string name);
+
     /**
      * Gets the description string. It makes debugging easier.
      * @return A string
