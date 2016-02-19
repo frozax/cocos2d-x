f23003f785a110f3ecdeb71fa4164f10c602a610
Francois Guibert <frozax@frozax.com>
2016-02-19 13:54:51 +0100

    rendertexture: use given filename directly

--- cocos/2d/CCRenderTexture.cpp
+++ cocos/2d/CCRenderTexture.cpp
@@ -508,7 +508,7 @@ bool RenderTexture::saveToFile(const std::string& fileName, Image::Format format
     
     _saveFileCallback = callback;
     
-    std::string fullpath = FileUtils::getInstance()->getWritablePath() + fileName;
+    std::string fullpath = fileName;
     _saveToFileCommand.init(_globalZOrder);
     _saveToFileCommand.func = CC_CALLBACK_0(RenderTexture::onSaveToFile, this, fullpath, isRGBA);
     
